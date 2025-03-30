09ab318d05a3178213fab9dd25f3400533d90822
SureshSingh80 <kumarsuresh8403@gmail.com>
2025-03-30 12:32:23 +0530

    changes successfully

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {Navbar} from '@/app/Navbar/page'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>
-          {/* <Navbar></Navbar> */}
-        </div>
+        
         {children}
       </body>
     </html>
